e4a594236809f4974b7e517bd28b796ee0f3d9c6
Kunal Umap <80446986+kunal-umap@users.noreply.github.com>
2022-02-05 01:02:02 +0530

    fixed Server issue

--- server/server.js
+++ server/server.js
@@ -1,12 +1,31 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
-const app = express()
+// // CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
+const cors = require('cors');
 
-app.get("/", function(req,res){
-    res.send("Hello")
+// used to process data sent through an HTTP request body.
+const http = require('http');
+
+// using dotenv for port variable and enviroment port in case of hosting
+require('dotenv').config();
+
+// setting port variable 
+const PORT = process.env.PORT || 8080;
+
+// creating express instance
+const app = express();
+app.use(express.json());
+
+// created server
+const server = http.createServer(app);
+
+// for cross connection 
+app.use(cors());
+
+app.get("/",(req,res) => {
+    res.send("Hello");
 })
 
-app.listen(4000,function(){
-    console.log("Server started on port 3000")
-})
+server.listen(PORT,()=>{
+    console.log(`Server started on http://localhost:${PORT}`);
+});
