4d3b975e3a1f7f22fd776413851471599a0c3fdc
Kunal Umap <80446986+kunal-umap@users.noreply.github.com>
2022-02-06 00:07:43 +0530

    setup hompage added route and folder stricture setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from './pages/home/HomePage';
+import LoginSignup from './pages/loginSignup/LoginSignup';
 function App() {
   return (
-    <div className="App">
-      <h1>React app</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route  path='/login' element={<LoginSignup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/loginSignup/LoginSignup.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function LoginSignup() {
+  return (
+    <div>
+        <h1>Login</h1>
+    </div>
+  );
+}
