df55bb759a173aff0e775e801824b6caa6b77654
Kunal Umap <80446986+kunal-umap@users.noreply.github.com>
2022-03-13 21:52:33 +0530

    created login form basic skeleton

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function HomePage() {
   return (
     <div>
         <h1>HOme Page</h1>
+        <Link to={"/login"}>Login</Link>
     </div>
   );
 }
--- client/src/pages/loginSignup/LoginSignup.jsx
+++ client/src/pages/loginSignup/LoginSignup.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './login.css';
 
 export default function LoginSignup() {
   return (
-    <div>
-        <h1>Login</h1>
+    <div className='loginPage'>
+      
+      <div className="loginCard">
+        <h1 className='loginHeading'>Login</h1>
+        <p className='loginDetail'>
+          Login to create your own link tree just with one click fully custoimizable
+        </p>
+        <div className="loginOption">
+
+        </div>
+      </div>
     </div>
   );
 }
