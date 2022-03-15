6d40de2049d8df17ed231dd4c9489de056edf435
Kunal Umap <80446986+kunal-umap@users.noreply.github.com>
2022-03-15 22:29:39 +0530

    Login page css completed

--- client/src/pages/loginSignup/LoginSignup.jsx
+++ client/src/pages/loginSignup/LoginSignup.jsx
@@ -4,14 +4,17 @@ import './login.css';
 export default function LoginSignup() {
   return (
     <div className='loginPage'>
-      
+      <div className="box box1"></div>
+      <div className="box box2"></div>
+      <div className="circle circle1"></div>
+      <div className="circle circle2"></div>
       <div className="loginCard">
         <h1 className='loginHeading'>Login</h1>
         <p className='loginDetail'>
           Login to create your own link tree just with one click fully custoimizable
         </p>
         <div className="loginOption">
-
+          <img src="" alt="" />
         </div>
       </div>
     </div>
