62065fd69165b90d998e0dde74d022480f1c24df
Kunal Umap <kunalgumap@gmail.com>
2022-04-04 14:58:32 +0530

    minor changes

--- client/src/pages/Data/Data.jsx
+++ client/src/pages/Data/Data.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import "./data.css";
 export default function Data() {
+
   return (
     <div className='data'>
-      
+      <form onSubmit={''}>
+
+        <button type="submit">Save</button>
+
+      </form>
     </div>
   )
 }
